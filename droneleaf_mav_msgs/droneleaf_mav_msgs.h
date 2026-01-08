0daeaa1ac74d857a64d43ddde228663847729ecf
engyaserothman <engyaserothman@gmail.com>
2026-01-08 15:57:31 +0400

    Refactor code structure for improved readability and maintainability

--- droneleaf_mav_msgs/droneleaf_mav_msgs.h
+++ droneleaf_mav_msgs/droneleaf_mav_msgs.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_DRONELEAF_MAV_MSGS.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_DRONELEAF_MAV_MSGS_XML_HASH -3975044455387559035
+#define MAVLINK_DRONELEAF_MAV_MSGS_XML_HASH 3918201758678581069
 
 #ifdef __cplusplus
 extern "C" {
