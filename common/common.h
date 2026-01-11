8c1d880496141ee73e75c804b1dfe989b5662318
engyaserothman <engyaserothman@gmail.com>
2026-01-11 21:54:36 +0400

    compilation of mavlink

--- common/common.h
+++ common/common.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_COMMON.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_COMMON_XML_HASH 5021675457338425225
+#define MAVLINK_COMMON_XML_HASH -8231177704785634383
 
 #ifdef __cplusplus
 extern "C" {
--- csAirLink/csAirLink.h
+++ csAirLink/csAirLink.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_CSAIRLINK.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_CSAIRLINK_XML_HASH -1335259667731467110
+#define MAVLINK_CSAIRLINK_XML_HASH -1380906010252283487
 
 #ifdef __cplusplus
 extern "C" {
