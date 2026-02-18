e882582be7e9b983530e8d51cbfd5e84cc7c568a
engyaserothman <engyaserothman@gmail.com>
2026-02-18 14:34:12 +0400

    generating def for all

--- cubepilot/cubepilot.h
+++ cubepilot/cubepilot.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_CUBEPILOT.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_CUBEPILOT_XML_HASH 8318682842704115969
+#define MAVLINK_CUBEPILOT_XML_HASH -8016417487414884535
 
 #ifdef __cplusplus
 extern "C" {
--- storm32/storm32.h
+++ storm32/storm32.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_STORM32.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_STORM32_XML_HASH 5586441533239136779
+#define MAVLINK_STORM32_XML_HASH -298930434074210726
 
 #ifdef __cplusplus
 extern "C" {
